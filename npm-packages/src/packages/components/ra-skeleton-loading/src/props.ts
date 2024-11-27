23fa66a0246ad218f131b32f1941cc56e0e3e41a
Redon Alla <redon.alla@gmail.com>
2024-11-27 20:50:03 +0100

    refactor(skeleton-loading): improve documentation and restructure imports
    Updated the `skeleton-loading` package to version 0.0.2, enhancing the documentation for better clarity and understanding. Moved dev dependencies from peerDependencies in `package.json`. Clarified comments and added detailed JSDoc annotations throughout the code to make it more maintainable and comprehensible. This refactor does not introduce any breaking changes or affect functionality but improves code readability and developer experience.

--- npm-packages/src/packages/components/ra-skeleton-loading/src/props.ts
+++ npm-packages/src/packages/components/ra-skeleton-loading/src/props.ts
@@ -2,13 +2,26 @@
  * @ Author: Redon Alla
  * @ Create Time: 2023-10-08 12:56:54
  * @ Modified by: Redon Alla
- * @ Modified time: 2024-10-17 20:38:25
+ * @ Modified time: 2024-11-27 17:01:13
  * @ Description: Prop for SkeletonLoader component.
  */
 
 import { ColorValue } from "react-native";
 import { SvgProps } from "react-native-svg";
 
+/**
+ * Interface for content loader properties.
+ * 
+ * @extends SvgProps
+ * 
+ * @property {boolean} [animate=true] - Boolean indicator whether to animate skeleton loading or not.
+ * @property {ColorValue} [backgroundColor] - Background color.
+ * @property {ColorValue} [foregroundColor] - Foreground color.
+ * @property {number} [speed=1.2] - Animation speed.
+ * @property {number} [interval=0.25] - Animation interval.
+ * @property {string} [uniqueKey] - Used for unique key for <Rect fill='' clipPath='' />.
+ * @property {JSX.Element} [beforeMask] - JSX.Element to render as mask on loading indicator.
+ */
 export interface IContentLoaderProps extends SvgProps {
   /**
    * Boolean indicator whether to animate skeleton loading or not.
--- npm-packages/src/packages/components/ra-skeleton-loading/src/svg-container.tsx
+++ npm-packages/src/packages/components/ra-skeleton-loading/src/svg-container.tsx
@@ -2,7 +2,7 @@
  * @ Author: Redon Alla
  * @ Create Time: 2023-10-08 12:58:28
  * @ Modified by: Redon Alla
- * @ Modified time: 2024-10-17 20:16:10
+ * @ Modified time: 2024-11-27 17:04:10
  * @ Description: SkeletonLoader container component.
  */
 
@@ -21,18 +21,102 @@ import { IContentLoaderProps } from './props';
 import { BACKGROUND_COLOR } from './constants';
 
 
+/**
+ * `AnimatedLinearGradient` is a component that combines the functionality of 
+ * `Animated` and `LinearGradient` to create an animated gradient effect.
+ *
+ * This component is created using `Animated.createAnimatedComponent` which 
+ * allows the `LinearGradient` component to be animated using the `Animated` API.
+ *
+ * @component
+ */
 const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
+/**
+ * Generates a unique identifier string.
+ *
+ * @returns {string} A unique identifier string.
+ */
 const uid = (): string => Math.random().toString(36).substring(6);
+
+/**
+ * Generates a unique identifier.
+ *
+ * @param {string} [uniqueKey] - An optional unique key to use. If not provided, a new unique identifier will be generated.
+ * @returns {string} The unique identifier.
+ */
 const uniqueId = (uniqueKey?: string) => uniqueKey || uid();
+
+/**
+ * Calculates the animation duration in milliseconds based on the given speed.
+ *
+ * @param speed - The speed factor to determine the duration.
+ * @returns The animation duration in milliseconds.
+ */
 const animationDuration = (speed: number): number => speed * 1000;
+
+/**
+ * Calculates the animation delay based on the given duration and interval.
+ *
+ * @param {number} duration - The duration of the animation.
+ * @param {number} interval - The interval between animations.
+ * @returns {number} - The calculated animation delay.
+ */
 const animationDelay = (duration: number, interval: number): number => interval * duration;
+
+/**
+ * Gets the color value.
+ *
+ * @param {ColorValue} themeColor - The theme color.
+ * @param {ColorValue} [color] - The color value.
+ * @returns {ColorValue} - The color value.
+ */
 const getColor = (themeColor: ColorValue, color?: ColorValue) => color || themeColor
 
-export default class extends React.Component<IContentLoaderProps> {
+/**
+ * SvgContainer is a React component that renders an SVG with an animated gradient.
+ * It uses the Animated API to create a smooth transition effect for the gradient.
+ * 
+ * @extends {React.Component<IContentLoaderProps>}
+ * 
+ * @property {boolean} [animate=true] - Determines if the animation should run.
+ * @property {number} [speed=1.2] - Speed of the animation in seconds.
+ * @property {number} [interval=0.25] - Interval between animation loops in seconds.
+ * 
+ * @method setAnimation - Sets up and starts the animation.
+ * @method componentDidMount - Lifecycle method that starts the animation if `animate` prop is true.
+ * @method componentDidUpdate - Lifecycle method that restarts the animation if `animate` prop changes to true.
+ * @method componentWillUnmount - Lifecycle method that sets the `unmounted` flag to true to stop the animation.
+ * 
+ * @param {IContentLoaderProps} props - The properties passed to the component.
+ * @param {React.ContextType<typeof ThemeContext>} context - The theme context used for styling.
+ * 
+ * @returns {JSX.Element} The rendered SVG element with animated gradient.
+ */
+export default class SvgContainer extends React.Component<IContentLoaderProps> {
+  /**
+   * Specifies the context type that this component subscribes to.
+   * This property is used to consume the `ThemeContext` within the component.
+   * 
+   * @type {React.Context<ThemeContextType>}
+   */
   static contextType = ThemeContext;
+
+  /**
+   * Declares a context variable that uses the type of the ThemeContext.
+   * This context is used to provide theming capabilities to the component.
+   * 
+   * @type {React.ContextType<typeof ThemeContext>}
+   */
   declare context: React.ContextType<typeof ThemeContext>
 
+  /**
+   * Default properties for the SVGContainer component.
+   * 
+   * @property {boolean} animate - Determines if the animation should be enabled.
+   * @property {number} speed - Speed of the animation.
+   * @property {number} interval - Interval between animation frames.
+   */
   static defaultProps = {
     animate: true,
     speed: 1.2,
@@ -45,6 +129,20 @@ export default class extends React.Component<IContentLoaderProps> {
   private idClip = `${this.fixedId}-diff`;
   private idGradient = `${this.fixedId}-animated-diff`;
 
+  /**
+   * Sets up and starts the animation for the component.
+   * 
+   * This method configures an animation using the `Animated.timing` function.
+   * It calculates the duration and delay for the animation based on the 
+   * `props.speed` and `props.interval` values. The animation runs with 
+   * `useNativeDriver` set to true for better performance.
+   * 
+   * Once the animation completes, it checks if the component is still mounted 
+   * and if the `animate` prop is true. If both conditions are met, it resets 
+   * the animated value and restarts the animation.
+   * 
+   * @private
+   */
   setAnimation = () => {
     //? props.speed is in seconds as it is compatible with web convert to milliseconds
     const durMs = animationDuration(this.props.speed!);
@@ -126,9 +224,9 @@ export default class extends React.Component<IContentLoaderProps> {
             y1={0}
             y2={0}
           >
-            <Stop offset={0} stopColor={getColor(BACKGROUND_COLOR[this.context.colorScheme!], backgroundColor)} />
+            <Stop offset={0} stopColor={getColor(BACKGROUND_COLOR[this.context.scheme!], backgroundColor)} />
             <Stop offset={0.5} stopColor={getColor(this.context.colors.card, foregroundColor)} />
-            <Stop offset={1} stopColor={getColor(BACKGROUND_COLOR[this.context.colorScheme!], backgroundColor)} />
+            <Stop offset={1} stopColor={getColor(BACKGROUND_COLOR[this.context.scheme!], backgroundColor)} />
           </AnimatedLinearGradient>
         </Defs>
       </Svg>
