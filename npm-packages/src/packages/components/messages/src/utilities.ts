3ac3ce80539ae8153ee9e9e8fa605adce17968b0
Redon Alla <redon.alla@gmail.com>
2025-01-01 21:14:58 +0100

    feat(messages): add getTextColor function to determine message text color
    Introduced a new utility function, getTextColor, to calculate the appropriate text color for message components based on theme brightness, message color type, and fill mode. Updated package version from `0.0.4` to `0.0.5` to reflect these changes.
    This enhancement improves theme adaptability and component styling flexibility.

--- npm-packages/src/packages/components/messages/src/utilities.ts
+++ npm-packages/src/packages/components/messages/src/utilities.ts
@@ -1,5 +1,7 @@
 import { ColorValue } from "react-native";
 import { FillMode } from "./props";
+import { WHITE_COLOR } from "./constants";
+import { Color } from "@flexnative/theme-context";
 
 /**
  * Returns the appropriate background color based on the Message Fill mode.
@@ -14,4 +16,31 @@ export function getBackgroundColor(colorValue: ColorValue, ghostOpacity: string,
     return `${colorValue.toString()}${ghostOpacity}`;
   
   return colorValue;
+}
+
+/**
+* Determines the text color based on the provided parameters.
+*
+* @param {boolean} isLight - Indicates if the theme is light.
+* @param {Color} color - The color type of the message component.
+* @param {ColorValue} colorValue - The color value to be used.
+* @param {FillMode} fill - Fill mode of component.
+* @param {ColorValue} blackColor - The color value representing black.
+* @returns {ColorValue} - The determined text color value.
+*/
+export function getTextColor(isLight: boolean, color: Color, colorValue: ColorValue, fill: FillMode, blackColor: ColorValue): ColorValue {
+  if(!isLight && (color === 'default' || color === 'secondary' || color === 'dark' || color === 'light'))
+    return WHITE_COLOR;
+
+  if (color === 'light' || color === 'default')
+    return blackColor;
+
+  if (fill === 'solid' && Boolean(color === 'secondary'))
+    return WHITE_COLOR;
+
+  return Boolean(isLight && color === 'default')
+    ? blackColor
+    : fill === 'solid'
+        ? WHITE_COLOR
+        : colorValue;
 }
