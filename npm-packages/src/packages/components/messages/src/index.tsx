12602125672b039cf597d21718710922b4ea0d69
Redon Alla <redon.alla@gmail.com>
2025-01-02 00:19:54 +0100

    feat(messages): enhance Message component with theme support and text rendering
    This update to the Message component adds integration with ThemeContext, allowing it to dynamically adapt styles based on current theme settings. Additionally, the component now renders `Text` elements for message display, improving usability by supporting direct text input within the props. The version is incremented from 0.0.5 to 0.0.11, reflecting significant updates. A new tsconfig.json file has been added to support TypeScript compilation with specific output and root directory configurations.

--- npm-packages/src/packages/components/messages/src/index.tsx
+++ npm-packages/src/packages/components/messages/src/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { MessageBoxProps } from "./props";
+import ThemeContext from "@flexnative/theme-context";
+import createStyles from "./styles";
 
+
+/**
+ * Represents a message box component.
+ * 
+ * The message box component is used to display messages to the user.
+ * 
+ * @extends React.PureComponent
+ */
 export default class Message extends React.PureComponent<MessageBoxProps> {
   /**
    * Default properties for the message box component.
@@ -15,15 +25,44 @@ export default class Message extends React.PureComponent<MessageBoxProps> {
    */
   static defaultProps = {
     radius: 'medium',
+    borderWidth: 'none',
+    fill: 'ghost',
     size: 'default',
     color: 'default',
-    type: 'ghost',
-    borderWidth: 'hairline',
   }
 
+  /**
+   * Specifies the context type for the component.
+   * This allows the component to subscribe to the nearest 
+   * ThemeContext provider and access its value.
+   */
+  
+  static contextType = ThemeContext;
+
+  /**
+   * Declares a context variable of the type inferred from the ThemeContext.
+   * This context is used to access the current theme settings within the component.
+   */
+  declare context: React.ContextType<typeof ThemeContext>;
+
   public render() {
+    const { radius, borderWidth, fill, size, color, borderColor, children, text, style, ...rest } = this.props;
+    const styles = createStyles({
+        radius,
+        borderWidth,
+        fill,
+        size,
+        color,
+        borderColor
+    }, this.context);
+        
     return (
-      <View>
+      <View style={[styles.container, style]} {...rest}>
+        {
+          Boolean(children) 
+            ? children
+            : <Text style={styles.text}>{text}</Text>
+        }
       </View>
     );
   }
